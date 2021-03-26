9239d56a981b30f9712ee28ea7ce5f07ac60886f
Reinis Danne <rei4dan@gmail.com>
2021-03-26 22:30:32 +0200

    GameTest: Implement tests for all public functions
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace RPSTest;
 
 use RPS\Game;
@@ -11,23 +13,71 @@ use RPS\Moves\Scissors;
 
 class GameTest extends TestCase
 {
-    public function testGame(): void
+    private array $allowedAnswers = [
+        Game::WINNER_DRAW,
+        Game::WINNER_PLAYER,
+        Game::WINNER_COMPUTER,
+    ];
+
+    private array $moveNames = [];
+
+    private Game $game;
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
+        parent::__construct($name, $data, $dataName);
+
         $moves = new Moves(
             new Rock(),
             new Paper(),
             new Scissors(),
         );
-        $game = new Game($moves);
 
-        $allowedAnswers = [
-            Game::WINNER_DRAW,
-            Game::WINNER_PLAYER,
-            Game::WINNER_COMPUTER,
-        ];
+        foreach ($moves as $move) {
+            $this->moveNames[] = $move->getName();
+        }
 
-        self::assertContains($game->play('rock'), $allowedAnswers);
-        self::assertContains($game->play('paper'), $allowedAnswers);
-        self::assertContains($game->play('scissors'), $allowedAnswers);
+        $this->game = new Game($moves);
+    }
+
+    public function possibleMoves(): array
+    {
+        $movesList = [];
+
+        foreach ($this->moveNames as $name) {
+            $movesList[] = [$name];
+        }
+
+        return $movesList;
+    }
+
+    /**
+     * @dataProvider possibleMoves
+     * @param string $userMove
+     */
+    public function testGame(string $userMove): void
+    {
+        self::assertContains($this->game->play($userMove), $this->allowedAnswers);
+    }
+
+    /**
+     * @dataProvider possibleMoves
+     * @param string $userMove
+     */
+    public function testGamePlay(string $userMove): void
+    {
+        $this->game->play($userMove);
+        $play = $this->game->getPlays()->getMove(1);
+
+        self::assertEquals($userMove, $play->getName());
+    }
+
+    public function testPossibleMoves(): void
+    {
+        $gameMoves = $this->game->getPossibleMoves();
+
+        foreach ($gameMoves as $gameMove) {
+            self::assertContains($gameMove->getName(), $this->moveNames);
+        }
     }
 }
